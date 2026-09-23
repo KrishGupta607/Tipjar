2e07a60dcd0cae4996ae7ae1bacece2bf7eaad94
KrishGupta607 <gupt1342@purdue.edu>
2026-09-23 16:53:56 -0400

    Update Tipjar.sol

--- Tipjar.sol
+++ Tipjar.sol
@@ -1,1 +1,24 @@
-
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+import "@openzeppelin/contracts@5.7.0/access/Ownable.sol";
+contract TipJar is Ownable{
+    mapping(address => uint256) public totalTipped;
+    uint256 public totalReceived;
+    constructor() Ownable(msg.sender) {}
+    event tipped(address indexed from, uint256 amount, uint256 newTotal);
+    
+    function deposit() external payable {
+        require(msg.value > 0, "zero tip");
+        totalReceived += msg.value;
+        totalTipped[msg.sender] += msg.value;
+        emit tipped(msg.sender, msg.value,  totalTipped[msg.sender]);
+    }
+    
+    function withdraw() external onlyOwner {
+        address ownerAddress = owner();
+        address payable destination = payable(ownerAddress);
+        uint256 amount = address(this).balance;
+        (bool ok, ) = destination.call{value: amount}("");
+        require(ok, "withdraw failed");
+    }
+}
